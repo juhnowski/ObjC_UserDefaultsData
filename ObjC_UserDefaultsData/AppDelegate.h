a64435e7a9f8e2040346c3a42351e1ff75d99fa0
Juhnowski <juhnowski@gmail.com>
2018-07-11 00:37:38 +0300

    Initial Commit

---
+++ ObjC_UserDefaultsData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjC_UserDefaultsData
+//
+//  Created by Илья Юхновский on 11.07.2018.
+//  Copyright © 2018 Илья Юхновский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjC_UserDefaultsData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjC_UserDefaultsData
+//
+//  Created by Илья Юхновский on 11.07.2018.
+//  Copyright © 2018 Илья Юхновский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
